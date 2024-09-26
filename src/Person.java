bccd8987928362605fa8afd84e9309f50ac5678b
Klementino75 <klementino75@ya.ru>
2024-09-26 21:15:56 +0300

    Added README.md

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Person {
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+
+    public Person(String name, String surname, String name1, String surname1) {
+        this.name = name1;
+        this.surname = surname1;
+    }
+
+    public Person(String name, String surname, int age, String name1, String surname1) {
+        this.name = name1;
+        this.surname = surname1;
+    }
+
+    public boolean hasAge() {
+        return false;
+    }
+    public boolean hasAddress() {
+        return false;
+    }
+
+    public String getName() {
+        return "";
+    }
+    public String getSurname() {
+        return "";
+    }
+    public OptionalInt getAge() {
+        return OptionalInt.empty();
+    }
+    public String getAddress() {
+        return "";
+    }
+
+    public void setAddress(String address) {  }
+    public void happyBirthday() {  }
+
+    public PersonBuilder newChildBuilder() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
--- src/PersonBuilder.java
+++ src/PersonBuilder.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class PersonBuilder {
+public class PersonBuilder {
+
+    public PersonBuilder setName(String name) {
+        return null;
+    }
+    public PersonBuilder setSurname(String surname) {
+        return null;
+    }
+    public PersonBuilder setAge(int age) {
+        return null;
+    }
+    public PersonBuilder setAddress(String address) {
+        return null;
+    }
+
+    public Person build() {
+        return null;
+    }
 }
